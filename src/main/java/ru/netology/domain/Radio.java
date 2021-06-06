5812dd9aa1609ad327b862c942bfaa0b0a0e84ce
Evgenii Semenov <eo.semenoff@gmail.com>
2021-06-06 13:11:57 +0300

    Radio and tests added

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,51 @@
+package ru.netology.domain;
+
+public class Radio {
+    private final int minFrequency = 0;
+    private final int maxFrequency = 9;
+    private int currentFrequency;
+    private final int minVolume = 0;
+    private final int maxVolume = 10;
+    private int currentVolume;
+
+    public void increaseFrequency() {
+        if (currentFrequency < maxFrequency) {
+            currentFrequency = currentFrequency + 1;
+        } else currentFrequency = 0;
+    }
+
+    public void decreaseFrequency() {
+        if (currentFrequency > minFrequency) {
+            currentFrequency = currentFrequency - 1;
+        } else currentFrequency = 9;
+    }
+
+    public int getCurrentFrequency() {
+        return currentFrequency;
+    }
+
+    public void setCurrentFrequency(int currentFrequency) {
+        if (currentFrequency > maxFrequency)
+            return;
+        if (currentFrequency < minFrequency)
+            return;
+        this.currentFrequency = currentFrequency;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < maxVolume) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > minVolume) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+}
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,122 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+//    @Test
+//    public void shouldGetAndSetRadioOn(){
+//        Radio radio = new Radio();
+//        boolean expected = false;
+//        radio.setRadioOn(expected);
+//        assertEquals(expected, radio.isRadioOn());
+//}
+
+    @Test
+    public void shouldIncreaseCurrentFrequencyUnderMaxFrequency() {
+        Radio radio = new Radio();
+        int currentFrequency = 8;
+        int expected = 9;
+        radio.setCurrentFrequency(currentFrequency);
+        radio.increaseFrequency();
+        assertEquals(expected, radio.getCurrentFrequency());
+    }
+
+    @Test
+    public void shouldIncreaseCurrentFrequencyEqualMaxFrequency() {
+        Radio radio = new Radio();
+        int currentFrequency = 9;
+        int expected = 0;
+        radio.setCurrentFrequency(currentFrequency);
+        radio.increaseFrequency();
+        assertEquals(expected, radio.getCurrentFrequency());
+    }
+
+    @Test
+    public void shouldIncreaseCurrentFrequencyEqualMinFrequency() {
+        Radio radio = new Radio();
+        int currentFrequency = 0;
+        int expected = 1;
+        radio.setCurrentFrequency(currentFrequency);
+        radio.increaseFrequency();
+        assertEquals(expected, radio.getCurrentFrequency());
+    }
+
+    @Test
+    public void shouldDecreaseCurrentFrequencyOverMinFrequency() {
+        Radio radio = new Radio();
+        int currentFrequency = 1;
+        int expected = 0;
+        radio.setCurrentFrequency(currentFrequency);
+        radio.decreaseFrequency();
+        assertEquals(expected, radio.getCurrentFrequency());
+    }
+
+    @Test
+    public void shouldDecreaseCurrentFrequencyEqualMinFrequency() {
+        Radio radio = new Radio();
+        int currentFrequency = 0;
+        int expected = 9;
+        radio.setCurrentFrequency(currentFrequency);
+        radio.decreaseFrequency();
+        assertEquals(expected, radio.getCurrentFrequency());
+    }
+
+    @Test
+    public void shouldDecreaseCurrentFrequencyEqualMaxFrequency() {
+        Radio radio = new Radio();
+        int currentFrequency = 9;
+        int expected = 8;
+        radio.setCurrentFrequency(currentFrequency);
+        radio.decreaseFrequency();
+        assertEquals(expected, radio.getCurrentFrequency());
+    }
+
+    @Test
+    public void shouldReturnCurrentFrequencyOverMaxFrequency() {
+        Radio radio = new Radio();
+        int currentFrequency = 10;
+        int expected = 0;
+        assertEquals(expected, radio.getCurrentFrequency());
+
+    }
+
+    @Test
+    public void shouldReturnCurrentFrequencyUnderMinFrequency() {
+        Radio radio = new Radio();
+        int currentFrequency = -1;
+        int expected = 0;
+        assertEquals(expected, radio.getCurrentFrequency());
+
+    }
+
+    @Test
+    public void shouldIncreaseCurrentVolumeUnderMaxVolume() {
+        Radio radio = new Radio();
+//        int currentVolume = 9;
+        int currentVolume = radio.getCurrentVolume();
+        int expected = 10;
+        radio.increaseVolume();
+        assertEquals(expected, radio.getCurrentVolume());
+
+
+    }
+//    @Test
+//    public void shouldIncreaseCurrentVolumeEqualMaxVolume() {
+//        Radio radio = new Radio();
+//        int currentVolume = 10;
+//        int expected = 10;
+//        radio.increaseVolume();
+//        assertEquals(expected, radio.getCurrentVolume());
+//
+//
+//    }
+}
+
+
+
+
+
+
+
